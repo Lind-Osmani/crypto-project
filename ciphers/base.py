2ea179dc02f65969a71f1e8f7059043c4a24c3d2
Lindi1111 <mjekiqil494@gmail.com>
2026-04-19 23:17:28 +0200

    base - m

--- ciphers/base.py
+++ ciphers/base.py
@@ -13,3 +13,13 @@ class BaseCipher(ABC):
     @abstractmethod
     def decrypt(self, text):
         pass
+        
+    def run(self):
+    text = input("Shkruaj tekstin: ")
+
+    encrypted = self.encrypt(text)
+    decrypted = self.decrypt(encrypted)
+
+    print("Original :", text)
+    print("Encrypted:", encrypted)
+    print("Decrypted:", decrypted)
